6c325e6599c9bb3eb645660a078e211f072a13cf
amar <amartele118@gmail.com>
2022-11-10 20:20:43 -1200

    good morning

---
+++ src/main/java/com/example/Webapps1Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapps1Application {
+
+	public static void main(String[] args) {
+		System.out.println("good morning");
+		SpringApplication.run(Webapps1Application.class, args);
+	}
+
+}
